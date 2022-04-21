89298dc3ab4e91818ed8422dfe263d18eb4a3dc0
Abimoldayeva Tomiris <abimoldayevat@gmail.com>
2022-04-21 08:28:20 +0600

    added lab6

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    // Declare an adapter
+    RecyclerView.Adapter programAdapter;
+    RecyclerView.LayoutManager layoutmanager;
+    // Next, prepare your data set. Create two string arrays for program name and program description respectively.
+    String[] programNameList = {"Vietnam", "USA", "Russia"};
+    String[] programDescriptionList = {"98.000.000", "320.000.000", "142.000.000"};
+    // Define an integer array to hold the image recourse ids
+    int[] programImages = {R.drawable.c, R.drawable.cplusplus, R.drawable.java};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Obtain a handle for the RecyclerView
+        recyclerView = findViewById(R.id.rvProgram);
+        // You may use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // Use a linear layout manager
+        layoutmanager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutmanager);
+        // Create an instance of ProgramAdapter. Pass context and all the array elements to the constructor
+        programAdapter = new ProgramAdapter(this, programNameList, programDescriptionList, programImages);
+        // Finally, attach the adapter with the RecyclerView
+        recyclerView.setAdapter(programAdapter);
     }
 }
